7d4876416fbff8541fd6fdbf08626f4545e8ac6e
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2026-03-05 11:06:40 -0700

    End of class

---
+++ HogWild/HogWildApp/Components/Pages/SamplePages/WorkingVersion.razor.cs
@@ -0,0 +1,39 @@
+﻿#nullable disable
+using HogWildSystem.BLL;
+using HogWildSystem.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace HogWildApp.Components.Pages.SamplePages
+{
+    public partial class WorkingVersion
+    {
+        #region Fields
+        //  Property for holding any feedback message
+        private string feedback;
+
+        //  This private field holds a reference to the WorkingVersionView instance
+        private WorkingVersionView workingVersionView = new();
+        #endregion
+
+        #region Properties
+        //  This attribute marks the property for dependecy injections
+        [Inject]
+        //  This property provides access to the "WorkingVersionService" service
+        protected WorkingVersionService WorkingVersionService { get; set; }
+        #endregion
+
+        #region Methods
+        private void GetWorkingVersion()
+        {
+            try
+            {
+                workingVersionView = WorkingVersionService.GetWorkingVersion();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
